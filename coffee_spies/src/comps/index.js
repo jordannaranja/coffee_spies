da61dd25d22736794cfbc1cef06478aff39f25e6
ESPIL_CEIL <quadclipse@gmail.com>
2021-03-14 21:21:29 -0700

    index

---
+++ coffee_spies/src/comps/index.js
@@ -0,0 +1,10 @@
+import Cancel from './Cancel';
+import CreatePostButton from './CreatePostButton';
+import EmailInput from './EmailInput';
+import PostInput from './PostInput';
+import PostDescInput from './PostDescInput';
+import ProfileSelect from './ProfileSelect';
+import ProfileUpload from './ProfileUpload';
+import LoginButton from './LoginButton';
+
+export {Cancel, CreatePostButton, EmailInput, PostInput, PostDescInput, ProfileSelect, ProfileUpload, LoginButton};
