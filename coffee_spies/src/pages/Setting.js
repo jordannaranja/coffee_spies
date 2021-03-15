8347f6b6513bbff1573e0730c11cf2b4445ea09d
ESPIL_CEIL <quadclipse@gmail.com>
2021-03-14 18:47:27 -0700

    Pages added

---
+++ coffee_spies/src/pages/Setting.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import styled from 'styled-components'; 
+
+const Button = styled.button`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px; 
+  align-item:center;
+  font-size: 27pt;
+
+  background: #FFFFFF;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  transform: rotate(45deg);
+`; 
+
+const Setting = () => {
+    return <Button>+</Button>
+}
+
+export default Setting;
