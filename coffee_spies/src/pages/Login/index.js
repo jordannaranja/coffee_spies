2fb388d0b85db5991c9c9ef1638fdc8bde833364
mathieuharvey <mathieuharvey@outlook.com>
2021-03-20 12:28:03 -0400

    finished css for both login and register

--- coffee_spies/src/pages/Login/index.js
+++ coffee_spies/src/pages/Login/index.js
@@ -9,39 +9,42 @@ import {
 import {useHistory} from 'react-router-dom';
 import styled from 'styled-components'; 
 import Logo from '../../images/coffee-spies-logo.png';
+import LoginImg from '../../images/login.jpg';
 
 //shortcut imports
-import {Add, Cancel, CreatePostButton, EmailInput, PostInput, PostDescInput, ProfileSelect, ProfileUpload, LoginButton} from '../../comps';
+import {Add, Cancel, CreatePostButton, EmailInput, PostInput, PostDescInput, ProfileSelect, ProfileUpload, LoginButton, RegisterNowButton} from '../../comps';
 
 const Container = styled.div`
-  display:flex;
-  align-item:center;
-  justify-content:center;
-  flex-direction:column;
-  width: 375px;
-  height:100vh;
-
-
-.subHeader{
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 21px;
-  margin-bottom:29px;
-  text-align: center;
-  color: #000000;
-}
-.header{
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 36px;
-  line-height: 42px;
-  text-align: center;
-  color: #000000;
-}
+display: flex;
+height: 100vh;
+width: 100vw;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+  background-image: linear-gradient(to top, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.25), rgba(255, 255, 255, 1)),
+  url('${LoginImg}');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  .header {
+    margin-bottom: 15px;
+  }
 `;
+
+  const Cont = styled.div`
+  display:flex;
+  justify-content: left;
+  flex-direction: column;
+  text-align: left;
+  `;
+
+  const CSLogo = styled.img`
+  margin-bottom: 50px;
+  `;
+
+  const LogIn = styled.div`
+  margin-top: 50px;
+  `;
 
 const Login = () => {
 
@@ -51,16 +54,20 @@ const Login = () => {
 
   const [error, setError] = useState(null);
   
-    return <Link to={"/main"}><Container>
-
-      <img src={Logo} />
-      <div className="header"> Welcome Back!
-      </div>
-
-      <EmailInput type="text" placeholder="Email" onChange={(e)=>setUn(e.target.value)} />
-      <EmailInput type="password" placeholder="Password" onChange={(e)=>setPw(e.target.value)} />
-      <LoginButton value="Submit" text="Sign In" />
-    </Container></Link>
+    return <Container>
+      <CSLogo src={Logo} />
+      <Cont>
+        <div className="header"><h1> Welcome Back!</h1></div>
+        <h2>Email</h2>
+        <EmailInput type="text" placeholder="Email" onChange={(e)=>setUn(e.target.value)} />
+        <h2>Password</h2>
+        <EmailInput type="password" placeholder="Password" onChange={(e)=>setPw(e.target.value)} />
+        <Link to={"/main"}><LoginButton value="Submit" text="Sign In" /></Link>
+      </Cont>
+      <LogIn>
+        <Link to={"/register"}><RegisterNowButton text="Register Now"/></Link>
+      </LogIn>
+    </Container>
 }
 
 export default Login;
--- coffee_spies/src/pages/Register/index.js
+++ coffee_spies/src/pages/Register/index.js
@@ -1,21 +1,70 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {useHistory} from 'react-router-dom';
 import styled from 'styled-components'; 
+import Logo from '../../images/coffee-spies-logo.png';
+import LoginImg from '../../images/login.jpg';
 
-const Button = styled.div`
-  position: absolute;
-  width: 50px;
-  height: 50px;
-  border-radius: 50px; 
-  align-item:center;
-  font-size: 27pt;
+//shortcut imports
+import {Add, Cancel, CreatePostButton, EmailInput, PostInput, PostDescInput, ProfileSelect, ProfileUpload, LoginButton, RegisterNowButton} from '../../comps';
 
-  background: #FFFFFF;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
-  transform: rotate(45deg);
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-image: linear-gradient(to top, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.25), rgba(255, 255, 255, 1)),
+  url('${LoginImg}');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  .header {
+    margin-bottom: 15px;
+  }
 `; 
 
+const Cont = styled.div`
+display:flex;
+justify-content: left;
+flex-direction: column;
+text-align: left;
+`;
+
+const CSLogo = styled.img`
+margin-bottom: 50px;
+`;
+
+const Reg = styled.div`
+margin-top: 50px;
+`;
+
 const Register = () => {
-    return <Button>+</Button>
+    return <Container>
+      <CSLogo src={Logo} />
+      <Cont>
+      <div className="header"><h1>Welcome to Coffee Spies!</h1></div>
+        <h2>Username</h2>
+        <EmailInput type="text" placeholder="Username"/>
+        <h2>Email</h2>
+        <EmailInput type="text" placeholder="Email"/>
+        <h2>Password</h2>
+        <EmailInput type="password" placeholder="Password"/>
+        <h2>Confirm Password</h2>
+        <EmailInput type="password" placeholder="Confirm Password"/>
+        <LoginButton value="Submit" text="Register" />
+      </Cont>
+      <Reg>
+      <Link to={"/"}><RegisterNowButton text="Log In"/></Link>
+      </Reg>
+    </Container>
 }
 
 export default Register;
