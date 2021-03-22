e20fd3fac838c34080c90a606c99fc38529f3734
espil <32521853+espil@users.noreply.github.com>
2021-03-21 17:09:53 -0700

    Revert "image upload on progress"

--- coffee_spies/src/comps/ProfileUpload/index.js
+++ coffee_spies/src/comps/ProfileUpload/index.js
@@ -1,8 +1,6 @@
 import React from 'react'; 
 import styled from 'styled-components'; 
 
-
-
 const Container = styled.div`
   min-width: 320px;
   height: 350px;   
@@ -24,44 +22,18 @@ const Button = styled.button`
 `;
 
 const ProfileUpload = () => {
-  const [img, setImage] = useState(null);
-
-  const onChange = (e) => { }
-  
-  const onClick = () => { }
-  const isSelectedImg = (e) => {
-      if (e.target.files !== null) {
-
-        const fd = new FormData();
-        fd.append('filename', e.target.files[0]);
-
-        axios
-          .post(`${URL}/user/profile-upload` , fd) 
-          .then(res => {
-            setImgUpload(res.data.imge_url);
-          })
-          .catch(error => {
-            console.log(error.response);
-          })
-
-      }
-    }
-  }
-
     return <Container>
 
-      <Button 
-       type="file" accept="image/jpeg, image/jpg" //uploadable file extensions
-      onChnage = {isSelectedImg} >
+      <Button>
       <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10.75 10.2464L16 4.99637L21.25 10.2464" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M16 19V5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M28 17V26C28 26.2652 27.8946 26.5196 27.7071 26.7071C27.5196 26.8946 27.2652 27 27 27H5C4.73478 27 4.48043 26.8946 4.29289 26.7071C4.10536 26.5196 4 26.2652 4 26V17" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
+<path d="M10.75 10.2464L16 4.99637L21.25 10.2464" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16 19V5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M28 17V26C28 26.2652 27.8946 26.5196 27.7071 26.7071C27.5196 26.8946 27.2652 27 27 27H5C4.73478 27 4.48043 26.8946 4.29289 26.7071C4.10536 26.5196 4 26.2652 4 26V17" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
       </Button>
 
     </Container>
-
+}
 
 ProfileUpload.defaultProps = {
 
