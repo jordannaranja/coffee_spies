e83b70d516f9268a253ecbeb57a32ff01b7f0258
ESPIL_CEIL <quadclipse@gmail.com>
2021-03-24 01:53:31 -0700

    minor change

--- coffee_spies/src/comps/ProfileUpload/index.js
+++ coffee_spies/src/comps/ProfileUpload/index.js
@@ -25,7 +25,6 @@ const Button = styled.button`
 const ProfileUpload = () => {
 
   const [file, setFile] = useState()
-  const [description, setDescription] = useState("")
   const [image, setImage] = useState()
 
   const submit = async event => {
@@ -33,7 +32,6 @@ const ProfileUpload = () => {
   
     const formData = new FormData()
     formData.append("image", file)
-    formData.append("description", description)
   
 
     const result = await axios.post('/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
